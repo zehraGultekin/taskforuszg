4422ab1440ad6151ef09ee04494d058444748849
zehraGltekin <zehragltekin12@gmail.com>
2025-08-21 14:02:20 +0300

    guncelleme

--- lib/features/task/task_view.dart
+++ lib/features/task/task_view.dart
@@ -26,54 +26,74 @@ class _TaskViewState extends State<TaskView> {
           ),
         ],
       ),
-      body: Column(
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Expanded(
-            child: GridView.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                crossAxisSpacing: 10.0,
-                mainAxisSpacing: 10.0,
-              ),
-              itemCount: 3,
-              itemBuilder: (context, index) {
-                return Container(
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.surface,
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Theme.of(context).colorScheme.shadow.withAlpha(
-                          50,
-                        ),
-                        blurRadius: 8,
-                        offset: const Offset(0, 3),
-                      ),
-                    ],
-                  ),
-                  alignment: Alignment.center,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.task,
-                        size: 40,
-                        color: Theme.of(context).colorScheme.onSurface,
-                      ),
-                      const SizedBox(height: 10),
-                      Text(
-                        'Görev ${index + 1}',
-                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                          color: Theme.of(context).colorScheme.onSurface,
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-              },
-            ),
-          )],
+          taskWidget(icon: Icons.calendar_view_day,number: "0",title: "Tamamlanmayı",subtitle: "Bekleyen",color: Colors.amber,),
+         SizedBox(width: 10),
+          taskWidget(icon: Icons.task_alt_outlined,title: "Başarıyla",subtitle: "Tamamlanan",number: "0",color: Colors.green,),
+        SizedBox(width: 10),
+         taskWidget(icon: Icons.person_add_disabled, title: "Alınmayı", subtitle: "Bekleyen", number: "5", color: Colors.grey)
+          ],
       )
+    );
+  }
+}
+
+class taskWidget extends StatelessWidget {
+final IconData icon ;
+final String number;
+final String title;
+  final String subtitle;
+  final Color color;
+  const taskWidget({
+  
+    super.key, required this.icon, required this.title, required this.subtitle, required this.number, required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 120,
+      width: 120,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Theme.of(context).colorScheme.surface,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: Offset(0, 3), 
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 4),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+          Icon(icon, size: 30, color: color),
+          SizedBox(height: 8),
+          Text(number,style: TextStyle(
+            color:color,
+            fontSize: 25,
+            fontWeight: FontWeight.bold
+          ),),
+          SizedBox(height: 8),
+          Text(title,style: TextStyle(
+            color: Colors.white,
+            fontSize: 12,
+          ),
+          ),
+          Text(subtitle,style: TextStyle(
+            color: Colors.white,
+            fontSize: 12,
+          ),)
+        
+                 ] ),
+      ),
     );
   }
 }
